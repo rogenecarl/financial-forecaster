20f193adab1f09ef52a022bc454a68db792db47b
rogenecarl <bajiglacostalesrosalijos2@gmail.com>
2026-02-06 09:29:43 +0800

    fix google Oauth login

--- src/actions/auth/google-auth-actions.ts
+++ src/actions/auth/google-auth-actions.ts
@@ -1,7 +1,9 @@
 import { authClient } from "@/lib/auth-client";
+import { DEFAULT_REDIRECT } from "@/config/auth";
 
 export const signInWithGoogle = async () => {
     await authClient.signIn.social({
-        provider: "google"
+        provider: "google",
+        callbackURL: DEFAULT_REDIRECT,
     })
 }
